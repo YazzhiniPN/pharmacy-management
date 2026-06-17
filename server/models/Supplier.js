110d44ba5bc40d58a57c4c74045c0e8bacf8a676
Yazzhini <pny86613@gmail.com>
2026-06-17 14:27:39 +0530

    Add Supplier Schema

---
+++ server/models/Supplier.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const supplierSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+    },
+    email: {
+        String
+    },
+    address : {
+        type: String
+    }
+},{
+    timestamps : true
+});
+
+const Supplier = mongoose.model("Supplier", supplierSchema);
+
+module.exports = Supplier;
+
